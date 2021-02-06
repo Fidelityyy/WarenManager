ddafcd5e29020ad43621c35b52e99067b67afc49
Fidel <fidel.tonak@stud.hs-hannover.de>
2021-02-06 15:13:16 +0100

    Initial commit

---
+++ app/src/main/java/com/tabian/saveanddisplaysql/EditDataActivity.java
@@ -0,0 +1,101 @@
+package com.tabian.saveanddisplaysql;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by User on 2/28/2017.
+ */
+
+public class EditDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "EditDataActivity";
+
+    private Button btnSave, btnDelete;
+    private EditText editable_item;
+    private EditText editPreis;
+    private EditText editAnzahl;
+
+    DatabaseHelper mDatabaseHelper;
+
+    private String selectedName;
+    private int selectedID;
+    private double selectedPreis;
+    private int selectedAnzahl;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_data_layout);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        editable_item = (EditText) findViewById(R.id.editable_item);
+        editPreis = (EditText) findViewById(R.id.editPreis);
+        editAnzahl = (EditText) findViewById(R.id.editAnzahl);
+
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        //get the intent extra from the ListDataActivity
+        Intent receivedIntent = getIntent();
+
+        //now get the itemID we passed as an extra
+        selectedID = receivedIntent.getIntExtra("id", -1); //NOTE: -1 is just the default value
+
+        //now get the name we passed as an extra
+        selectedName = receivedIntent.getStringExtra("name");
+
+        selectedPreis = receivedIntent.getDoubleExtra("preis", 0);
+
+        selectedAnzahl = receivedIntent.getIntExtra("anzahl", 0);
+
+        //set the text to show the current selected name
+        editable_item.setText(selectedName);
+        editAnzahl.setText(Integer.toString(selectedAnzahl));
+        editPreis.setText(Double.toString(selectedPreis));
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String item = editable_item.getText().toString();
+                String preis = editPreis.getText().toString();
+                String anzahl = editAnzahl.getText().toString();
+                if (!item.equals("") || !preis.equals("") || !anzahl.equals("")) {
+                    mDatabaseHelper.updateName(item, selectedID, selectedName, selectedPreis, selectedAnzahl);
+                    Intent intent = new Intent(EditDataActivity.this, ListDataActivity.class);
+                    startActivity(intent);
+                } else {
+                    toastMessage("Alle Felder ausfüllen!");
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDatabaseHelper.deleteName(selectedID, selectedName, selectedPreis, selectedAnzahl);
+                editable_item.setText("");
+                editAnzahl.setText("");
+                editPreis.setText("");
+                toastMessage("Aus der Datenbank entfernt.");
+                Intent intent = new Intent(EditDataActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    /**
+     * customizable toast
+     *
+     * @param message
+     */
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/tabian/saveanddisplaysql/MainActivity.java
@@ -0,0 +1,76 @@
+package com.tabian.saveanddisplaysql;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd, btnViewData;
+    private EditText produktNameFeld, anzahlFeld, preisFeld;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        produktNameFeld = (EditText) findViewById(R.id.produktName);
+        anzahlFeld = (EditText) findViewById(R.id.anzahlFeld);
+        preisFeld = (EditText) findViewById(R.id.preisFeld);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnViewData = (Button) findViewById(R.id.btnView);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = produktNameFeld.getText().toString();
+                int anzahl = Integer.parseInt(anzahlFeld.getText().toString());
+                double preis = Double.parseDouble(preisFeld.getText().toString());
+                if (produktNameFeld.length() != 0 || anzahlFeld.length() != 0 || preisFeld.length() != 0) {
+                    AddData(newEntry, anzahl, preis);
+                    produktNameFeld.setText("");
+                    anzahlFeld.setText("");
+                    preisFeld.setText("");
+                } else {
+                    toastMessage("Alle Felder ausfüllen!");
+                }
+
+            }
+        });
+
+        btnViewData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void AddData(String newEntry, int anzahl, double preis) {
+        boolean insertData = mDatabaseHelper.addData(newEntry, anzahl, preis);
+
+        if (insertData) {
+            toastMessage("Daten zur Datenbank hinzugefügt!");
+        } else {
+            toastMessage("Irgendwas ist schief gelaufen!");
+        }
+    }
+
+    /**
+     * customizable toast
+     *
+     * @param message
+     */
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
